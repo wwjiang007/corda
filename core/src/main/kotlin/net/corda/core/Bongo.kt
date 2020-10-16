0f81cdb6f52ba61af6aefd1a7d4909753eb35913
lwogan <lorcan.wogan@r3.com>
2020-10-16 15:08:02 +0100

    add scr and guava for osgi testing

---
+++ core/src/main/kotlin/net/corda/core/Bongo.kt
@@ -0,0 +1,34 @@
+package net.corda.core;
+
+import com.google.common.collect.ImmutableMap
+import com.google.common.collect.Lists
+import org.osgi.service.component.annotations.Activate
+import org.osgi.service.component.annotations.Component
+import org.osgi.service.component.annotations.Reference
+import org.osgi.service.log.Logger
+import org.osgi.service.log.LoggerFactory
+import java.util.function.Consumer
+
+@Component(immediate = true)
+class Bongo @Activate constructor(
+        @Reference(service = LoggerFactory::class)
+        private val logger: Logger
+) {
+    init {
+        System.err.println("-------------- Initialising Bongo!")
+        logger.info("Started {}", this::class.java)
+    }
+
+    @Activate
+    fun doIt() {
+        //some guava code to trigger osgi imports
+        val items: Map<*, *> = ImmutableMap.of("coin", 3, "glass", 4, "pencil", 1)
+        val fruits: List<String> = Lists.newArrayList("orange", "banana", "kiwi",
+                "mandarin", "date", "quince")
+
+        System.err.println("-------------- Activated Bongo!")
+        throw InternalError("BOOM!")
+    }
+}
+
+
