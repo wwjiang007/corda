76d0cd5776d84a5aadce2c8deb7b8ab191ce1561
Alex-Koller-R3 <alex.koller@r3.com>
2021-04-14 09:55:58 +0100

    Signer signs

--- tools/network-cloner/src/main/kotlin/net/corda/networkcloner/impl/IdentityMapperImpl.kt
+++ tools/network-cloner/src/main/kotlin/net/corda/networkcloner/impl/IdentityMapperImpl.kt
@@ -2,6 +2,8 @@ package net.corda.networkcloner.impl
 
 import net.corda.networkcloner.api.IdentityMapper
 import net.corda.networkcloner.entity.Identity
+import net.corda.nodeapi.internal.crypto.getCertificateAndKeyPair
+import net.corda.nodeapi.internal.crypto.getSupportedKey
 import java.io.File
 import java.io.FileInputStream
 import java.security.KeyPair
@@ -20,8 +22,8 @@ class IdentityMapperImpl : IdentityMapper {
         val ks  = KeyStore.getInstance("JKS")
         ks.load(FileInputStream(File("/Users/alex.koller/Projects/contract-sdk/examples/test-app/buildDestination/nodes/Operator/certificates/nodekeystore.jks")),"cordacadevpass".toCharArray())
 
-        val identityPrivateKeyEntry = ks.getEntry("identity-private-key", KeyStore.PasswordProtection("cordacadevpass".toCharArray())) as KeyStore.PrivateKeyEntry
+        val x = ks.getCertificateAndKeyPair("identity-private-key", "cordacadevpass")
 
-        return listOf(Identity(KeyPair(identityPrivateKeyEntry.certificate.publicKey, identityPrivateKeyEntry.privateKey)))
+        return listOf(Identity(x.keyPair))
     }
 }
