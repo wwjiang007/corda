99f80977a7b821dbbc613b549da751d418a39b1a
Alex-Koller-R3 <alex.koller@r3.com>
2021-04-18 12:09:11 +0100

    Introduce IdentityFactory

--- tools/network-cloner/src/main/kotlin/net/corda/networkcloner/impl/NodesToNodesMigrationTaskFactory.kt
+++ tools/network-cloner/src/main/kotlin/net/corda/networkcloner/impl/NodesToNodesMigrationTaskFactory.kt
@@ -1,11 +1,14 @@
 package net.corda.networkcloner.impl
 
 import net.corda.networkcloner.api.MigrationTaskFactory
+import net.corda.networkcloner.entity.MigrationTask
 import java.io.File
 
-class NodesToNodesMigrationTaskFactory(source : File, destination : File) : MigrationTaskFactory {
+class NodesToNodesMigrationTaskFactory(val source : File, val destination : File) : MigrationTaskFactory {
 
-    override fun getMigrationTasks() {
-        //todo
+    override fun getMigrationTasks() : List<MigrationTask> {
+        val sourceParties = NodesDirectoryPartyRepository(source)
+        val destinationParties = NodesDirectoryPartyRepository(destination)
+        return emptyList()
     }
 }
