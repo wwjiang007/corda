d10317e6be7a30ed93bbf88e08388d4431125542
Alex-Koller-R3 <alex.koller@r3.com>
2021-05-10 21:17:50 +0100

    Clean up

--- tools/network-cloner/src/main/kotlin/net/corda/networkcloner/NetworkCloner.kt
+++ tools/network-cloner/src/main/kotlin/net/corda/networkcloner/NetworkCloner.kt
@@ -36,46 +36,4 @@ fun main(args: Array<String>) {
     migrationTasks.filterNot { it.identity.sourceParty.toString().contains("notary", true) }.forEach {
         DefaultMigration(it, serializer, signer, cordappsRepository, true).run()
     }
-
-/*
-    val cordappLoader = CordappsRepositoryImpl(File("/Users/alex.koller/Projects/contract-sdk/examples/test-app/buildDestination/nodes/Operator/cordapps"))
-    val serializer = SerializerImpl(cordappLoader.getCordappLoader())
-    val transactionStore = NodeDatabaseImpl("jdbc:h2:/Users/alex.koller/Projects/contract-sdk/examples/test-app/buildSource/nodes/Operator/persistence", "sa", "", { _ -> throw UnsupportedOperationException()}, { _ -> throw java.lang.UnsupportedOperationException()})
-    val migrationData = transactionStore.readMigrationData()
-    val signer = SignerImpl()
-
-
-    migrationData.transactions.forEach {
-        val deserialized = serializer.deserializeDbBlobIntoTransaction(it.transaction)
-
-        val wTx = (deserialized as SignedTransaction).coreTransaction as WireTransaction
-
-        //edit the component groups start
-
-        val componentGroups =
-            createComponentGroups(wTx.inputs, wTx.outputs, wTx.commands, wTx.attachments, wTx.notary, wTx.timeWindow, wTx.references, wTx.networkParametersHash)
-
-
-        wTx.componentGroups.forEachIndexed {
-            i, v ->
-            val componentGroupCreated = componentGroups[i]
-            println("Are they the same for ${v.groupIndex}: ${componentGroupCreated.components == v.components}")
-        }
-
-        //edit the component groups end
-
-        val destinationWireTransaction = WireTransaction(componentGroups, wTx.privacySalt, wTx.digestService)
-
-
-        val sTx = SignedTransaction(destinationWireTransaction, deserialized.sigs)
-
-        val serializedFromSignedTx = sTx.serialize(context = SerializationDefaults.STORAGE_CONTEXT.withEncoding(CordaSerializationEncoding.SNAPPY))
-
-        println("Db tx and destination tx are same: ${it.transaction.contentEquals(serializedFromSignedTx.bytes)}")
-
-    }
-
-    println("Done")
-
-*/
 }
