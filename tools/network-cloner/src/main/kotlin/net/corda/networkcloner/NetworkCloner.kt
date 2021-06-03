59a70ab5eba660bb23df116f63a482a0487c6038
Alex-Koller-R3 <alex.koller@r3.com>
2021-06-03 11:44:29 +0100

    Add time taken

--- tools/network-cloner/src/main/kotlin/net/corda/networkcloner/NetworkCloner.kt
+++ tools/network-cloner/src/main/kotlin/net/corda/networkcloner/NetworkCloner.kt
@@ -6,6 +6,7 @@ import net.corda.networkcloner.impl.SignerImpl
 import net.corda.networkcloner.impl.NodesToNodesMigrationTaskFactory
 import net.corda.networkcloner.runnable.DefaultMigration
 import java.io.File
+import java.util.*
 
 fun main(args: Array<String>) {
     val sourceDir = File(args[0])
@@ -17,6 +18,7 @@ fun main(args: Array<String>) {
     require(pathToCordapps.exists() && pathToCordapps.isDirectory) {"$pathToCordapps must exist and be a directory"}
 
     println("The migration is commencing from $sourceDir to $destDir")
+    val startTime = Date().time
     val cordappsRepository = CordappsRepositoryImpl(pathToCordapps, 1, 3)
 
     val migrationTaskFactory = NodesToNodesMigrationTaskFactory(sourceDir, destDir, cordappsRepository)
@@ -28,4 +30,6 @@ fun main(args: Array<String>) {
     migrationTasks.filterNot { it.identity.sourceParty.toString().contains("notary", true) }.forEach {
         DefaultMigration(it, serializer, signer, cordappsRepository, false).call()
     }
+    val endTime = Date().time
+    println("Time taken is ${(endTime - startTime).toDouble() / 1000} seconds")
 }
